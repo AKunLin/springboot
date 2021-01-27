92cc45bf7a6e8495b3fd19398a0ae9c33795b7de
Luakun <10718792@qq.com>
2021-01-27 17:11:43 +0800

    整合Redis

--- src/main/java/com/luakun/service/impl/UserServiceImpl.java
+++ src/main/java/com/luakun/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.luakun.dao.UserMapper;
 import com.luakun.pojo.User;
 import com.luakun.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,9 +20,23 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @Override
     public List<User> findAllUser() {
-        List<User> userList = userMapper.findAllUser();
-        return userList;
+        //1.获取redis中的数据
+        List<User> list = (List<User>) redisTemplate.boundValueOps("key_all").get();
+        //2.判断 是否有，如果有则返回，如果没有则从mysql中获取设置到redis中再返回
+        if (list != null && list.size() > 0) {
+            return list;
+        }
+        List<User> allUser = userMapper.findAllUser();
+
+        //3 从mysql中获取设置到redis中再返回
+        redisTemplate.boundValueOps("key_all").set(allUser);
+
+        return allUser;
     }
+
 }
